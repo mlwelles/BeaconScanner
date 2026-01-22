3c1b18cfd6cc3295f6d0ecf30291d9d8c57b1eda
Michael Welles <mlwelles@gmail.com>
2026-01-21 23:02:49 -0500

    Add tests for Help menu configuration
    - Test CFBundleHelpBookFolder is configured
    - Test CFBundleHelpBookName is configured
    - Test help bundle exists in app resources
    - Test help index.html exists
    - Test help content contains expected keywords

--- BeaconScannerTests/BeaconScannerTests.m
+++ BeaconScannerTests/BeaconScannerTests.m
@@ -53,4 +53,68 @@
     XCTAssertFalse([[HGBeaconScanner sharedBeaconScanner] scanning], @"Manager can stop scanning");
 }
 
+#pragma mark - Help Menu Tests
+
+- (void)testHelpBookFolderConfigured
+{
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *helpBookFolder = [bundle objectForInfoDictionaryKey:@"CFBundleHelpBookFolder"];
+    XCTAssertNotNil(helpBookFolder, @"CFBundleHelpBookFolder should be configured in Info.plist");
+    XCTAssertEqualObjects(helpBookFolder, @"BeaconScanner.help", @"Help book folder should be BeaconScanner.help");
+}
+
+- (void)testHelpBookNameConfigured
+{
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *helpBookName = [bundle objectForInfoDictionaryKey:@"CFBundleHelpBookName"];
+    XCTAssertNotNil(helpBookName, @"CFBundleHelpBookName should be configured in Info.plist");
+    XCTAssertEqualObjects(helpBookName, @"nyc.welles.BeaconScanner.help", @"Help book name should match bundle identifier");
+}
+
+- (void)testHelpBundleExists
+{
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *helpBookFolder = [bundle objectForInfoDictionaryKey:@"CFBundleHelpBookFolder"];
+    XCTAssertNotNil(helpBookFolder, @"Help book folder must be configured");
+
+    NSString *helpPath = [[bundle resourcePath] stringByAppendingPathComponent:helpBookFolder];
+    BOOL isDirectory = NO;
+    BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:helpPath isDirectory:&isDirectory];
+
+    XCTAssertTrue(exists, @"Help bundle should exist at %@", helpPath);
+    XCTAssertTrue(isDirectory, @"Help bundle should be a directory");
+}
+
+- (void)testHelpIndexExists
+{
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *helpBookFolder = [bundle objectForInfoDictionaryKey:@"CFBundleHelpBookFolder"];
+    XCTAssertNotNil(helpBookFolder, @"Help book folder must be configured");
+
+    NSString *helpPath = [[bundle resourcePath] stringByAppendingPathComponent:helpBookFolder];
+    NSString *indexPath = [helpPath stringByAppendingPathComponent:@"Contents/Resources/en.lproj/index.html"];
+
+    BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:indexPath];
+    XCTAssertTrue(exists, @"Help index.html should exist at %@", indexPath);
+}
+
+- (void)testHelpContentNotEmpty
+{
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *helpBookFolder = [bundle objectForInfoDictionaryKey:@"CFBundleHelpBookFolder"];
+    XCTAssertNotNil(helpBookFolder, @"Help book folder must be configured");
+
+    NSString *helpPath = [[bundle resourcePath] stringByAppendingPathComponent:helpBookFolder];
+    NSString *indexPath = [helpPath stringByAppendingPathComponent:@"Contents/Resources/en.lproj/index.html"];
+
+    NSError *error = nil;
+    NSString *content = [NSString stringWithContentsOfFile:indexPath encoding:NSUTF8StringEncoding error:&error];
+
+    XCTAssertNil(error, @"Should be able to read help content: %@", error);
+    XCTAssertNotNil(content, @"Help content should not be nil");
+    XCTAssertGreaterThan(content.length, 100, @"Help content should have substantial content");
+    XCTAssertTrue([content containsString:@"BeaconScanner"], @"Help content should mention BeaconScanner");
+    XCTAssertTrue([content containsString:@"iBeacon"], @"Help content should mention iBeacon");
+}
+
 @end
